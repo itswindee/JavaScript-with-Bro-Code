798fc5b59a5c5061f1d4e9e239f0dedb6c4c4f05
windee <ezhu0922@gmail.com>
2024-07-18 20:58:31 -0400

    updated

---
+++ 5. Type Conversion/index.js
@@ -0,0 +1,8 @@
+// type conversion = change the datatype of a value to another
+//                  (strings, numbers, booleans)
+
+let age = window.prompt("How old are you?");
+age = Nunber(age);
+age+=1;
+
+console.log(age);
